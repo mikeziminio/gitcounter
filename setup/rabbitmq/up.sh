756a8a3cd6cae474dbfaf0b025e548f1768d3066
Mike Zimin <mikeziminio@gmail.com>
2023-12-01 16:15:49 +0400

    docker

--- setup/rabbitmq/up.sh
+++ setup/rabbitmq/up.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Получение абсолютного пути к папке текущего скрипта
+dir_name=$(dirname "$(realpath "$0")")
+
 # Инициализация env vars
-source $dir_name/../../.env
+source "$dir_name/../../.env"
 
 # Передаю именно так, потому что не хочу делать export
 RABBITMQ_USER=$RABBITMQ_USER \
@@ -9,3 +12,5 @@ RABBITMQ_PASSWORD=$RABBITMQ_PASSWORD \
 RABBITMQ_VHOST=$RABBITMQ_VHOST \
 RABBITMQ_PORT=$RABBITMQ_PORT \
 envsubst < "$dir_name/rabbitmq.conf.template" > "$dir_name/rabbitmq.conf"
+
+cp "$dir_name/rabbitmq.conf" /etc/rabbitmq/rabbitmq.conf
